9a824ae0be4cb84848d619d084353cac547224da
cole-tucker <coletucker@email.arizona.edu>
2021-02-10 20:07:59 -0700

    might work

--- practicum/week-2/question9.js
+++ practicum/week-2/question9.js
@@ -9,5 +9,9 @@ output: 'hello'
 */
 
 var suffleString = function(input, shuffleIndices) {
-
+    var finalArray = new Array(shuffleIndices.length).fill(0);
+    for (let i = 0; i < shuffleIndices.length; i++) {
+        finalArray[shuffleIndices[i]] = input[shuffleIndices[i]];
+    }
+    return finalArray;
 };
