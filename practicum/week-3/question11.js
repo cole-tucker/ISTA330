93c23864bba394327cf9d2d0cbfea12afb3244ee
cole-tucker <coletucker@email.arizona.edu>
2021-02-21 17:52:44 -0700

    maybe this will do it

--- practicum/week-3/question11.js
+++ practicum/week-3/question11.js
@@ -16,16 +16,12 @@ output: 1
 var d_count = function(input1, input2, d) {
     count = 0
     for (let i = 0; i < input1.length; i++) {
-        var cond = false;
         for (let j = 0; j < input2.length; j++) {
             if (Math.abs(input1[i]-input2[j]) > d) {
-                var cond = true;
+                count += 1;
                 break;
             }
         }
-        if (cond == false) {
-            count += 1;
-        }
     }
-    return count;
+    return input1.length - count;
 };
