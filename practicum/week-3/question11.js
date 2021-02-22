d1e70df97859b710692721f879225a559c626f20
cole-tucker <coletucker@email.arizona.edu>
2021-02-21 17:55:37 -0700

    forgot to change condition

--- practicum/week-3/question11.js
+++ practicum/week-3/question11.js
@@ -17,7 +17,7 @@ var d_count = function(input1, input2, d) {
     count = 0
     for (let i = 0; i < input1.length; i++) {
         for (let j = 0; j < input2.length; j++) {
-            if (Math.abs(input1[i]-input2[j]) > d) {
+            if (Math.abs(input1[i]-input2[j]) <= d) {
                 count += 1;
                 break;
             }
